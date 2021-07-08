57ca7751451fc1afe65abccc903682a27dee181d
Pam Gaiguer <pamella.gaiguer@gmail.com>
2021-07-08 16:38:45 +1000

    about us - missing style

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,44 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { CommonModule, } from '@angular/common';
-import { BrowserModule  } from '@angular/platform-browser';
-import { Routes, RouterModule } from '@angular/router';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { ComponentsComponent } from './components/components.component';
-import { ProfileComponent } from './pages/profile/profile.component';
-import { SignupComponent } from './pages/signup/signup.component';
 import { NucleoiconsComponent } from './components/nucleoicons/nucleoicons.component';
-import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
-import { StaffComponent } from './pages/staff/staff.component';
-import { ClientsComponent } from './pages/clients/clients.component';
-import { TestimonialsComponent } from './pages/testimonials/testimonials.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 import { ServicesComponent } from './pages/services/services.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
-const routes: Routes =[
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'components',                           component: ComponentsComponent },
-    { path: 'profile',                              component: ProfileComponent },
-    { path: 'signup',                               component: SignupComponent },
-    { path: 'nucleoicons',                          component: NucleoiconsComponent },
-    { path: 'home',                                 component: HomeComponent },
-    { path: 'about',                                component: AboutComponent },
-    { path: 'contact',          component: ContactComponent },
-    { path: 'staff',          component: StaffComponent },
-    { path: 'clients',          component: ClientsComponent },
-    { path: 'testimonials',          component: TestimonialsComponent },
-    { path: 'services',          component: ServicesComponent },
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'components', component: ComponentsComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'nucleoicons', component: NucleoiconsComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'services', component: ServicesComponent },
 ];
 
 @NgModule({
   imports: [
     CommonModule,
     BrowserModule,
-    RouterModule.forRoot(routes,{
-      useHash: false
-    })
+    RouterModule.forRoot(routes, {
+      useHash: false,
+    }),
   ],
-  exports: [
-  ],
+  exports: [],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,36 +1,24 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
-
-import { HomeComponent } from './home/home.component';
-import { ProfileComponent } from './profile/profile.component';
-import { SignupComponent } from './signup/signup.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-import { StaffComponent } from './staff/staff.component';
-import { TestimonialsComponent } from './testimonials/testimonials.component';
-import { ClientsComponent } from './clients/clients.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
 import { ServicesComponent } from './services/services.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        FormsModule,
-        NgbModule,
-        RouterModule,
-    ],
-    declarations: [
-        HomeComponent,
-        SignupComponent,
-        ProfileComponent,
-        AboutComponent,
-        ContactComponent,
-        StaffComponent,
-        TestimonialsComponent,
-        ClientsComponent,
-        ServicesComponent
-    ]
+  imports: [CommonModule, FormsModule, NgbModule, RouterModule],
+  declarations: [
+    HomeComponent,
+    SignupComponent,
+    ProfileComponent,
+    AboutComponent,
+    ContactComponent,
+    ServicesComponent,
+  ],
 })
-export class PagesModule { }
+export class PagesModule {}
